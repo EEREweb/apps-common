5d76595eb3572fa82ee1ccad2954607c1d08cb51
Ravi Gaddipati <rgaddipa@gmail.com>
2019-02-27 14:05:55 -0500

    Update funclib.js
    add forEach definition

--- js/funclib.js
+++ js/funclib.js
@@ -11,6 +11,13 @@ if (!String.prototype.repeat) {
 		return Array(n+1).join(this);
 	}
 }
+if (!Array.prototype.forEach) {
+    Array.prototype.forEach = function(fn, scope) {
+        for(var i = 0, len = this.length; i < len; ++i) {
+            fn.call(scope, this[i], i, this);
+        }
+    }
+}
 
 // custom JS functions
 
